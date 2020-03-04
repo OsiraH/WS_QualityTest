88ddc4c0fce2980c9906e3e61eb9f9c0bef44007
kristiyanparlikov <56762245+kristiyanparlikov@users.noreply.github.com>
2020-03-04 11:45:08 +0200

    Update TestGetTimeBoughtInMinutes.java

--- src/test/TestGetTimeBoughtInMinutes.java
+++ src/test/TestGetTimeBoughtInMinutes.java
@@ -1,10 +1,11 @@
 package test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.After;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,20 +23,53 @@ class TestGetTimeBoughtInMinutes {
 	}
 
 	@Test
-	void testGet20MinFor50EUR() throws IllegalCoinException {
+	void testGet20MinFor50cents() throws IllegalCoinException {
 		
 		// Arrange
-		int expectedParkingTime = 20;	// In minutes
-		int coinValue = 50;
+		int coinValue = 50; // Cents
 		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
 		Currency.ValidCoinType coinType = Currency.ValidCoinType.FRACTION;
+		int expectedParkingTime = 20;	// In minutes
 		
 		// Act
 		payStationCTR.addPayment(coinValue, coinCurrency, coinType);
 			
 		// Assert
-		assertEquals(expectedParkingTime, payStationCTR.readDisplay(), "Should display 20 min for 50 EUR");
+		assertEquals(expectedParkingTime, payStationCTR.readDisplay(), "Should display 20 min for 50 cents");
 
+	}
+	
+	@Test
+	void testGet107MinFor20dkk() throws IllegalCoinException {
+		
+		//Arrange 
+		int coinValue = 20; // DKK
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.DKK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		int expectedParkingTime = 107;	// In minutes
+		
+		// Act
+		payStationCTR.addPayment(coinValue, coinCurrency, coinType);
+			
+		// Assert
+		assertEquals(expectedParkingTime, payStationCTR.readDisplay(), "Should display 107 min for 20 DKK");
+	}
+	
+	@Test	
+	void  testMultipleCoinsInsertedConversion() throws IllegalCoinException {
+		//Arrange
+		ArrayList<Integer> coinValues = new ArrayList<>(
+			    Arrays.asList(2, 5, 2, 1)); //DKK
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.DKK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		int expectedParkingTime = 54; // In minutes
+		//Act
+		for(int i : coinValues) {
+			payStationCTR.addPayment(i, coinCurrency, coinType);
+		}
+		
+		//Assert
+		assertEquals(expectedParkingTime, payStationCTR.readDisplay(), "Should display n min for 10 DKK");
 	}
 	
 	@After
@@ -44,3 +78,5 @@ class TestGetTimeBoughtInMinutes {
 	}
 
 }
+
+
